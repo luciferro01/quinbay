3f6e735099d8a15d59a25ea2855b23ab4e291381
Mohil Bansal <lucifer1111ronaldo@gmail.com>
2025-02-22 13:20:05 +0530

    chore: Adding basic structures and fixing some initial bugs and making compatible with intellij

---
+++ assessment/src/CsvFileHandler.java
@@ -0,0 +1,12 @@
+public class CsvFileHandler implements MyFileHandler {
+
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void write() {
+
+    }
+}
--- week0/CsvFileHandler.java
+++
@@ -1,3 +0,0 @@
-public class CsvFileHandler implements MyFileHandler {
-
-}
